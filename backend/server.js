37e73d61c2bda2855ed82a8c6317533937c0e488
Al Martinez <al@sitparker.com>
2018-12-27 13:46:27 -0500

    deploy

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,8 @@ app.use(express.static(path.join(__dirname, '/../build')));
 
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/../build/index.html'));
+    const index = path.join(__dirname, 'build', 'index.html');
+  res.sendFile(index);
 });
 
 
