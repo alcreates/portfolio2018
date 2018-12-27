339207adbf5c448096e1eddc04da89dbe5a8bdce
Al Martinez <al@sitparker.com>
2018-12-27 13:52:01 -0500

    new build

---
+++ build/precache-manifest.21d2debeeb4631d118ed61a811f43e98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "61aea107874b8d63af9c",
+    "url": "/static/js/main.61aea107.chunk.js"
+  },
+  {
+    "revision": "719949752634e94e4768",
+    "url": "/static/js/1.71994975.chunk.js"
+  },
+  {
+    "revision": "61aea107874b8d63af9c",
+    "url": "/static/css/main.f5f7716b.chunk.css"
+  },
+  {
+    "revision": "1ec0388638e6b21ba813051ee91a00a5",
+    "url": "/index.html"
+  }
+];
